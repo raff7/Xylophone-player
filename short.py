78aaa1f23ee7b7a01b43b0e2dde008903febc5a7
kret0w <aleksandrawypych@gmail.com>
2017-12-12 18:02:04 +0100

    Add files via upload

--- short.py
+++ short.py
@@ -2,26 +2,38 @@ import numpy as np
 import numpy.ma as ma
 import matplotlib.pyplot as plt
 import statistics
+import wave
+import soundfile as sf
+import math
 
+preX = wave.open('xylo.wav','r')
+xylo = preX.readframes(-1)
+xylo = np.fromstring(xylo, 'Int16')
+print xylo
+preP = wave.open('pianoD1.wav','r')
+piano = preX.readframes(-1)
+piano = np.fromstring(piano, 'Int16')
+#fs = spf.getframerate()
+#np.set_printoptions(threshold=np.inf)
 
 
 #this is sofrt fuoier trnasfrom
 def short(X):
     #length of the signal
     n = len(X)
-    print n
+
     # window
     # the window, so basically which part of the orginal signal will be processed by fourier transfrom
     win  = n/10
-    print win
+
     #hop size
     #how much do we move, so if you see this the windows will overlap, which is one of the basis of sft
     hop = win/2
-    print hop
+
     spectrumArray = [0]*n
     fourierArray = [0] * n
     Jup = []
-    print Jup
+
     j = 0
 
 # again magic, so for each of the window you perform normal fourier, but here instead of doing i+1, you use hop, to skip some
@@ -40,11 +52,15 @@ def short(X):
         #Jup = Jup + aaa
         #print  lo
         #print Jup
-
+        print "diidid", i
         #Jup = Jup + aaa
         Jup = Jup + aaa
-    print spectrumArray
-    print fourierArray
+
+    #print spectrumArray
+    #print "hrjhrhte", spectrumArray[0]
+    #length = len(spectrumArray[0])
+    #print len(length)
+    #print fourierArray
     return Jup
 
 
@@ -82,13 +98,55 @@ def half(X):
             Y.append(X[i])
             #np.insert(Y,X[i])
     return Y
+    
+    j times i should be the overall time 
 """
 
-dupa = short(X)
-print len(X)
-print len(fupa)
-plt.plot(dupa)
-plt.show()
+def spectralPeaks(fourier, power):
+    length=len(fourier)
+    spectralPeaksStorage=[0]*length
+    for i in range(0,length,2):
+        peakPower=0
+        windowFourier = fourier[i]
+        windowPower = power[i]
+        windowLength=len(windowPower)
+
+        print windowLength
+        #windowLength=len(windowFourier)
+        for j in range (0,windowLength):
+            spectralPeak = 0
+            if (windowPower[j]>peakPower):
+                peakPower=windowPower[j]
+                spectralPeaksStorage[i] = windowFourier[j]
+
+        #do timining later (you get it from location in spectral peaks when freq x starts
+        # --> onset __ don't play anything else until new node starts
+    return spectralPeaksStorage
+
+def compareNote(X,Y):
+    sameNote=False
+    if (X[0]==Y[0]):
+        sameNote=True
+    return sameNote
+
+a,xyloPower,xyloFourier = short(xylo)
+b,pianoPower,pianoFourier= short(piano)
+print "done with fu"
+xyloPeaks=spectralPeaks(xyloFourier,xyloPower)
+print "done with xylopeaks"
+pianoPeaks=spectralPeaks(pianoFourier,pianoPower)
+print "done with pianopeaks"
+
+same=compareNote(pianoPeaks,xyloPeaks)
+print "I am dumb",same
+
+
+#dupa = short(X)
+#print len(X)
+#print len(fupa)
+#plt.plot(dupa)
+#plt.show()
+
 
 '''''''''
 #SPECTRUM SHOULD BE DONE SEPERETELY FOR EACH SEGMENT/WINDOW
